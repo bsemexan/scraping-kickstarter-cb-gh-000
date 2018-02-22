975b7521fc354bb622a1024c36ea612dad9aef68
Berry  Semexan <bsemexan@gmail.com>
2018-02-22 03:16:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,9 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
   binding.pry
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
